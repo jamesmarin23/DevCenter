89dfa3831c144016b5ece965cbb5ba7203572608
MayraFernandaVargas <89797646+MayraFernandaVargas@users.noreply.github.com>
2021-10-04 08:26:28 -0500

    cambios

--- devcenter_gestor_ventas/src/Vistas/parts/BuscarProducto.js
+++ devcenter_gestor_ventas/src/Vistas/parts/BuscarProducto.js
@@ -20,71 +20,82 @@ export class BuscarProducto extends Component {
 
                                     <div className="row">
                                         <div className="col-lg-12 mb-2 mt-2">
-                                            <GridTable showRowsInformation={false} isPaginated={false} columns={[
-                                                {
-                                                    id: 1,
-                                                    field: "Codigo",
-                                                    label: "Codigo"
-                                                },
-                                                {
-                                                    id: 2,
-                                                    field: "Nombre",
-                                                    label: "Nombre"
-                                                },
-                                                {
-                                                    id: 3,
-                                                    field: "Categoria",
-                                                    label: "Categoria"
-                                                },
-                                                {
-                                                    id: 4,
-                                                    field: "Marca",
-                                                    label: "Marca"
-                                                },
-                                                {
-                                                    id: 5,
-                                                    field: "Valor",
-                                                    label: "Valor"
-                                                }
-                                            ]}
-                                                rows={[
-                                                    {
-                                                        Codigo: 1,
-                                                        Nombre: "Manzana",
-                                                        Categoria: "Frutas",
-                                                        Marca: "Frescampo",
-                                                        Valor: 2500
-                                                    },
-                                                ]} />
+                                            <div className="input-group">
+                                                <div className="input-group-prepend">
+                                                    <div className="input-group-text">
+                                                        <i className="mdi mdi-magnify"></i>
+                                                    </div>
+                                                </div>
+                                                <input type="text" className="form-control" placeholder="Buscar" name="producto" required="" />
+                                            </div>
                                         </div>
 
-                                        <div className="col-lg-12 mt-2">
-                                            <button className="btn btn-success btn-block font-weight-bold">
+                                        {/* <div className="col">
+                                            <button className="btn btn-success  font-weight-bold">
                                                 <i className="mdi mdi-magnify"></i>
                                                 {" Buscar Producto"}
                                             </button>
-                                        </div>
+                                        </div> */}
                                     </div>
+
+                                    <table class="table table-bordered table-sm">
+                                        <thead>
+                                            <th scope="col">Codigo</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Descripcion</th>
+                                            <th scope="col">Categoria</th>
+                                            <th scope="col">Marca</th>
+                                            <th scope="col">Valor</th>
+                                        </thead>
+                                        <tbody>
+                                            <th scope="row">1</th>
+                                            <td>Manzana</td>
+                                            <td>Manzana Roja</td>
+                                            <td>Frutas</td>
+                                            <td>Frescampo</td>
+                                            <td>2500</td>
+                                        </tbody>
+                                    </table>
+
+                                    {/* <GridTable showRowsInformation={false} isPaginated={false} columns={[
+                                        {
+                                            id: 1,
+                                            field: "Codigo",
+                                            label: "Codigo"
+                                        },
+                                        {
+                                            id: 2,
+                                            field: "Nombre",
+                                            label: "Nombre"
+                                        },
+                                        {
+                                            id: 3,
+                                            field: "Categoria",
+                                            label: "Categoria"
+                                        },
+                                        {
+                                            id: 4,
+                                            field: "Marca",
+                                            label: "Marca"
+                                        },
+                                        {
+                                            id: 5,
+                                            field: "Valor",
+                                            label: "Valor"
+                                        }
+                                    ]}
+                                        rows={[
+                                            {
+                                                Codigo: 1,
+                                                Nombre: "Manzana",
+                                                Categoria: "Frutas",
+                                                Marca: "Frescampo",
+                                                Valor: 2500
+                                            },
+                                        ]} /> */}
                                 </div>
                             </div>
                         </div>
-                        
-                        {/* <table class="table table-bordered table-sm">
-                            <thead>
-                                <th scope="col">Codigo</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Categoria</th>
-                                <th scope="col">Marca</th>
-                                <th scope="col">Valor</th>
-                            </thead>
-                            <tbody>
-                                <th scope="row">1</th>
-                                <td>Manzana</td>
-                                <td>Frutas</td>
-                                <td>Frescampo</td>
-                                <td>2500</td>
-                            </tbody>
-                        </table> */}
                     </div>
                 </div>
             </div>
